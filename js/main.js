3ced4bcc4e7a98c46678c38cb1e40ec48f7a12da
kaayoung <20190856@sungshin.ac.kr>
2022-09-02 12:23:07 +0900

    init html css

---
+++ js/main.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const $next_arrow = document.querySelector(".course-arrow_next") ;
+const $prev_arrow = document.querySelector(".course-arrow_prev") ;
+const $course_item_container = document.querySelector(".course-item_container") ;
